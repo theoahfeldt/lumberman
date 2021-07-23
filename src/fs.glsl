b2e37c19c9bcc32ce7822fca5ea4cf71370d8922
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2021-07-23 14:46:47 +0200

    Improve lighting in shader

--- src/fs.glsl
+++ src/fs.glsl
@@ -6,9 +6,17 @@ out vec4 frag_color;
 uniform sampler2D tex;
 
 void main() {
-  vec3 obj_color = vec3(texture(tex, v_uv));
-  vec3 light_dir = vec3(0., -0.5, -1.);
-  float kd = dot(v_normal, -light_dir);
+  vec3 light_color = vec3(1., 1., 1.);
+  vec3 light_dir = normalize(vec3(0., 0.5, 1.));
 
-  frag_color = vec4(obj_color * kd, 1.);
+  float ambient_strength = 0.2;
+  vec3 ambient = ambient_strength * light_color;
+
+  vec3 obj_color = vec3(texture(tex, v_uv));
+
+  float diff = max(dot(v_normal, light_dir), 0.0);
+  vec3 diffuse = diff * light_color;
+
+  vec3 result = (ambient + diffuse) * obj_color;
+  frag_color = vec4(result, 1.);
 }
